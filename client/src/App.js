a80e8bf15d5b0ddf984cca0afd83ae845196488b
unknown <vanekh.avramov@gmail.com>
2022-03-05 13:47:52 +0300

    feat: prepear react template

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-<<<<<<< HEAD
-  console.log('test')
-=======
-  console.log('asdasd')
->>>>>>> 58046bc2bfa13580a1bec917e29270bc6ebe5848
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
