fb17cee4c3289785b2cd77b9cc2f866ca348cfaf
unknown <vanekh.avramov@gmail.com>
2022-03-07 14:06:32 +0300

    feat: add nav links

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import styles from "../src/styles/main.module.scss"
-import Devices from "../src/components/device/Devices"
+import styles from "../src/styles/app.module.scss"
+// import Devices from "../src/components/device/Devices"
 import Header from "./components/Header";
-import Brands from "./components/brand/Brands";
-import Types from "./components/type/Types";
-
+// import Brands from "./components/brand/Brands";
+// import Types from "./components/type/Types";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Catalog from "./components/pages/Catalog";
+import Basket from "./components/pages/Basket";
 
 function App() {
   return (
     <div className={styles.container}>
       <Header />
       <div className={styles.container_inner}>
-        <div className={styles.topT}>
-          <Types />
-        </div>
-        <div className={styles.botL}>
-          <Brands />
-        </div>
-        <div className={styles.botR}>
-          <Devices />
-        </div>
+        <Routes>
+          <Route path={"/"} exact element={<Home />} />
+          <Route path={"/catalog"} element={<Catalog />} />
+          <Route path={"/basket"} element={<Basket />} />
+        </Routes>
       </div>
     </div>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from '../styles/header.module.scss'
+import React from "react";
+import styles from "../styles/header.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       Header
+      <nav className={styles.nav}>
+        <NavLink className={styles.nav_item} extend="true" aria-current="page" to={"/"}>
+          Home
+        </NavLink>
+        <NavLink className={styles.nav_item} to={"/catalog"}>Catalog</NavLink>
+        <NavLink className={styles.nav_item} to={"/basket"}>Basket</NavLink>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
